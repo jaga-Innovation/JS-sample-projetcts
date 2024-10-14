66b8771cbbe778799115b9a10112180f86c251ef
Jagadeshwaran <148187521+jaga-Innovation@users.noreply.github.com>
2024-10-14 16:16:27 +0530

    Update app.js

--- modules/app.js
+++ modules/app.js
@@ -1,15 +1,15 @@
 // app.js
 
-import { PI, add, subtract } from './mathUtils.js';
+import ipdi from './mathUtils.js';
 
 const resultsDiv = document.getElementById('results');
 
 // Using the imported functions and constant
-const sum = add(10, 5);
-const difference = subtract(10, 5);
+const sum = ipdi.add(10, 5);
+const difference = ipdi.sub(10, 5);
 
 resultsDiv.innerHTML = `
-    <p>PI: ${PI}</p>
+    <p>PI: ${ipdi.PI}</p>
     <p>Sum: ${sum}</p>
     <p>Difference: ${difference}</p>
 
