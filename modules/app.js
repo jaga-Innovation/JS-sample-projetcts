c57c31bfe9cb673b12a340eb452747a8be940723
Jagadeshwaran <148187521+jaga-Innovation@users.noreply.github.com>
2024-10-15 08:07:38 +0530

    npm install lodash

--- modules/app.js
+++ modules/app.js
@@ -1,21 +1,29 @@
-// app.js
 
-import ipdi from './mathUtils.js';
+import imageSize from 'image-size'
 
-import axios from 'axios'
+console.log(imageSize);
 
-const resultsDiv = document.getElementById('results');
+
+// const resultsDiv =ize document.getElementById('results');
 
 // Using the imported functions and constant
-const sum = ipdi.add(10, 5);
-const difference = ipdi.sub(10, 5);
+// const sum = ipdi.add(10, 5);
+// const difference = ipdi.sub(10, 5);
 
-resultsDiv.innerHTML = `
-    <p>PI: ${ipdi.PI}</p>
-    <p>Sum: ${sum}</p>
-    <p>Difference: ${difference}</p>
+// resultsDiv.innerHTML = `
+//     <p>PI: ${ipdi.PI}</p>
+//     <p>Sum: ${sum}</p>
+//     <p>Difference: ${difference}</p>
 
-`;
+// `;
+
+imageSize("./computer.png", function (err, dimensions) {
+  console.log(dimensions.width, dimensions.height)
+})
+
+
+console.log(imageSize);
+
 
 
 
